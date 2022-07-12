e35c3486f6f5978ae1e5a3225968563eb99f9976
Sreejit7 <sreejitde7@gmail.com>
2022-07-12 23:51:42 +0530

    updated footer and added separate CNCF logos based on theme
    Signed-off-by: Sreejit7 <sreejitde7@gmail.com>

--- src/App/Footer/Footer.tsx
+++ src/App/Footer/Footer.tsx
@@ -1,16 +1,30 @@
-import { CNCFBadge } from "./CNCFBadge";
 import "./footer.scss";
+import cncfIconDark from "../../assets/cncf-dark.svg";
+import cncfIconLight from "../../assets/cncf-light.svg";
+import { useThemeContext } from "../../common/context/theme";
 
 export const Footer = () => {
+  const {
+    themeContext: { darkScheme },
+  } = useThemeContext();
+
   return (
-    <>
-      <CNCFBadge />
-      <footer className="footer">
-        © 2022 <a href=".">OpenFeature</a> Powered by{" "}
+    <footer className="footer">
+      <img
+        src={darkScheme ? cncfIconDark : cncfIconLight}
+        alt="CNCF"
+        className="cncf-logo"
+      />
+      <p className="footer-text">
+        © 2022 <a href=".">OpenFeature</a> is a{" "}
+        <a href="https://www.cncf.io" target="_blank" rel="noreferrer">
+          Cloud Native Computing Foundation
+        </a>{" "}
+        sandbox project Powered by{" "}
         <a href="https://netlify.com" target="_blank" rel="noreferrer">
           Netlify
         </a>
-      </footer>
-    </>
+      </p>
+    </footer>
   );
 };
