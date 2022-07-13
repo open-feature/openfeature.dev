8bf1c80edd079820c1a999ecd6cd21095a59acb3
Sreejit7 <sreejitde7@gmail.com>
2022-07-13 21:16:43 +0530

    updated footer text & styling for large & small devices
    Signed-off-by: Sreejit7 <sreejitde7@gmail.com>

--- src/App/Footer/Footer.tsx
+++ src/App/Footer/Footer.tsx
@@ -15,16 +15,22 @@ export const Footer = () => {
         alt="CNCF"
         className="cncf-logo"
       />
-      <p className="footer-text">
-        © 2022 <a href=".">OpenFeature</a> is a{" "}
-        <a href="https://www.cncf.io" target="_blank" rel="noreferrer">
-          Cloud Native Computing Foundation
-        </a>{" "}
-        sandbox project Powered by{" "}
-        <a href="https://netlify.com" target="_blank" rel="noreferrer">
-          Netlify
-        </a>
-      </p>
+      <span className="footer-text">
+        <span>
+          © 2022 <a href=".">OpenFeature</a> is a{" "}
+          <a href="https://www.cncf.io" target="_blank" rel="noreferrer">
+            Cloud Native Computing Foundation
+          </a>{" "}
+          sandbox project{" "}
+        </span>
+        <span className="footer-text-divider">|</span>{" "}
+        <span>
+          This site is powered by{" "}
+          <a href="https://netlify.com" target="_blank" rel="noreferrer">
+            Netlify
+          </a>
+        </span>
+      </span>
     </footer>
   );
 };
