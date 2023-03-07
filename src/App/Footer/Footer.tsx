4b057b0fe50c5edfe2d65f528568ddeda0b988e7
Michael Beemer <beeme1mr@users.noreply.github.com>
2023-03-06 21:39:56 -0500

    chore: bump the copyright year
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- src/App/Footer/Footer.tsx
+++ src/App/Footer/Footer.tsx
@@ -17,7 +17,7 @@ export const Footer = () => {
       />
       <span className="footer-text">
         <span>
-          © 2022 <a href=".">OpenFeature</a> is a{" "}
+          © 2023 <a href=".">OpenFeature</a> is a{" "}
           <a href="https://www.cncf.io" target="_blank" rel="noreferrer">
             Cloud Native Computing Foundation
           </a> (<a href="https://linuxfoundation.org/trademark-usage">CNCF®</a>){" "}
