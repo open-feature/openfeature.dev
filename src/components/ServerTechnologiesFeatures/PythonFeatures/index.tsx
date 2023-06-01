7f12ccb559ded566a0767fef6b7a8bbd9e1cb1d2
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2023-06-02 08:41:10 +1000

    remove unused imports
    Signed-off-by: Adam Gardner <26523841+agardnerIT@users.noreply.github.com>

--- src/components/ServerTechnologiesFeatures/PythonFeatures/index.tsx
+++ src/components/ServerTechnologiesFeatures/PythonFeatures/index.tsx
@@ -1,12 +1,6 @@
 import { faPython } from '@fortawesome/free-brands-svg-icons';
 import React from 'react';
 import { OpenFeatureTechnologiesPage } from '../../custom/OpenFeatureTechnologiesPage';
-import FlagdSvg from '@site/static/img/flagd-no-fill.svg';
-import SplitSvg from '@site/static/img/split-no-fill.svg';
-import CloudbeesSvg from '@site/static/img/cloudbees-no-fill.svg';
-import OTelSvg from '@site/static/img/otel-no-fill.svg';
-import DatadogSvg from '@site/static/img/datadog-no-fill.svg';
-import CheckCircle from '@site/static/img/check-circle-no-fill.svg';
 
 export class PythonFeatures extends React.Component {
   override render() {
