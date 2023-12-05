32919b7920290204cd1402d6b1f17e30a3299a0a
Michael Beemer <beeme1mr@users.noreply.github.com>
2023-12-05 09:54:25 -0500

    fixed export issue
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

---
+++ src/datasets/sdks/php.ts
@@ -0,0 +1,10 @@
+import { SDK } from '.';
+
+export const PHP: SDK = {
+  name: 'PHP',
+  category: 'Server',
+  repo: 'php-sdk',
+  logoKey: 'go-no-fill.svg',
+  technology: 'PHP',
+  href: '/docs/reference/technologies/server/php',
+};
