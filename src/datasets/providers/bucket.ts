6e89ad37a69faefcbb8ae1a6bda951711af0f9f6
Michael Beemer <beeme1mr@users.noreply.github.com>
2024-09-11 09:40:59 -0400

    add bucketco to ecosystem (#674)
    ## This PR
    
    - adds Bucket to the ecosystem
    
    ### Notes
    
    I've removed the fill from the SVG logo so that it works in both light
    and dark modes.
    
    Direct link to the preview:
    https://deploy-preview-674--openfeature.netlify.app/ecosystem?instant_search%5BrefinementList%5D%5Bvendor%5D%5B0%5D=Bucket
    
    ---------
    
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

---
+++ src/datasets/providers/bucket.ts
@@ -0,0 +1,16 @@
+import BucketSvg from '@site/static/img/bucketco-no-fill.svg';
+
+import { Provider } from '.';
+
+export const Bucket: Provider = {
+  name: 'Bucket',
+  logo: BucketSvg,
+  technologies: [
+    {
+      technology: 'JavaScript',
+      vendorOfficial: true,
+      href: 'https://github.com/bucketco/bucket-javascript-sdk/tree/main/packages/openfeature-node-provider',
+      category: ['Server'],
+    },
+  ],
+};
