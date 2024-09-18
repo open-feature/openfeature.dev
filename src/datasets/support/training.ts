82316927c46bfb37f3db7e69a139c620de6e32be
Michael Beemer <beeme1mr@users.noreply.github.com>
2024-09-18 15:36:59 -0400

    fix typo
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- src/datasets/support/training.ts
+++ src/datasets/support/training.ts
@@ -12,7 +12,7 @@ type ResourcesType = {
 export const TrainingResources: ResourcesType[] = [
   {
     title: 'OpenFeature Fundamentals',
-    description: 'An introduction to then core concepts of OpenFeature',
+    description: 'An introduction to the core concepts of OpenFeature',
     href: 'https://www.youtube.com/watch?v=heQ83k15ZE4&list=PLiQt8D1ofl8zs2zoiMNI8WRdNQ8GUy84I',
     iconDefinition: faYoutube,
   },
