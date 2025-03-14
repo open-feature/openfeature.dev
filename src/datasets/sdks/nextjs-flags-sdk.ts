98f49ea1526dbe130e9cd769c4fcc1e7f338dbaa
Jonathan Norris <jonathan@taplytics.com>
2025-03-14 14:14:36 -0400

    feat: rename technology to Next.js
    Signed-off-by: Jonathan Norris <jonathan@taplytics.com>

--- src/datasets/sdks/nextjs-flags-sdk.ts
+++ src/datasets/sdks/nextjs-flags-sdk.ts
@@ -1,14 +1,14 @@
 import { SDK } from '.';
 
 export const NextjsFlagsSDK: SDK = {
-  name: 'Next.js Flags',
-  description: 'Vercel Next.js Flags SDK with OpenFeature Adapter',
+  name: 'Vercel Flags',
+  description: 'Vercel Flags SDK with OpenFeature Adapter',
   filename: 'javascript/nextjs-flags-sdk',
   category: 'Client',
   slug: 'nextjs-flags-sdk',
   branch: 'main',
   logoKey: 'nextjs-no-fill.svg',
-  technology: 'Next.js Flags SDK',
+  technology: 'Next.js',
   parentTechnology: 'JavaScript',
   skipParentTechnologyProviders: true,
   href: 'https://flags-sdk.dev/docs/api-reference/adapters/openfeature',
