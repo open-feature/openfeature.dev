5993d0784508ca22314f7d7a490f0e0d77969cec
Michael Beemer <beeme1mr@users.noreply.github.com>
2025-03-27 19:56:30 +0000

    fix: avoid generating SDK docs for non-OpenFeature SDKs
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- scripts/process-sdk-readmes.ts
+++ scripts/process-sdk-readmes.ts
@@ -22,7 +22,7 @@ const carriageReturnsToNewLines = (content: string): string => {
 const removeEmojisFromHeaders = (content: string): string => {
   return content.replace(
     /(#{1,5}) ([\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g,
-    '$1'
+    '$1',
   );
 };
 
@@ -181,10 +181,13 @@ const markdownProcessor = (sdks: SDK[]) => {
  * Transforms SDK READMEs for inclusion in the OpenFeature docs.
  */
 export const processSdkReadmes = {
-  paths: SDKS.map((sdk) => {
-    const branch = sdk.branch ?? 'main';
-    const folder = sdk.folder ?? '';
-    return `${sdk.repo}/${branch}${folder}/README.md`;
-  }),
+  paths: SDKS
+    // Only include SDKs with a repo. This signifies that the SDK is part of the OpenFeature org.
+    .filter((sdk) => sdk.repo)
+    .map((sdk) => {
+      const branch = sdk.branch ?? 'main';
+      const folder = sdk.folder ?? '';
+      return `${sdk.repo}/${branch}${folder}/README.md`;
+    }),
   modifyContent: markdownProcessor(SDKS),
 };
