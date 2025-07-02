9895dbc168c582dce7fa944bd90503bdc3a8d054
Michael Beemer <beeme1mr@users.noreply.github.com>
2025-07-02 09:23:01 -0400

    Add Bucketeer Koltin and Go Providers
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- src/datasets/providers/bucketeer.ts
+++ src/datasets/providers/bucketeer.ts
@@ -13,10 +13,22 @@ export const Bucketeer: Provider = {
       category: ['Client'],
     },
     {
+      technology: 'Kotlin',
+      vendorOfficial: true,
+      href: 'https://github.com/bucketeer-io/openfeature-kotlin-client-sdk',
+      category: ['Client'],
+    },
+    {
       technology: 'JavaScript',
       vendorOfficial: true,
       href: 'https://www.npmjs.com/package/@bucketeer/openfeature-js-client-sdk',
       category: ['Client'],
+    },
+    {
+      technology: 'Go',
+      vendorOfficial: true,
+      href: 'https://github.com/bucketeer-io/openfeature-go-server-sdk',
+      category: ['Server'],
     },
   ],
 };
