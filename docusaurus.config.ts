66a9faacedbbf06ab796319e3ce82f025dba42e2
Jonathan Norris <jonathan@taplytics.com>
2025-10-29 17:03:01 -0400

    docs: update ofrep-api link
    Signed-off-by: Jonathan Norris <jonathan@taplytics.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -293,7 +293,7 @@ const config: Config = {
           {
             id: 'ofrep-api',
             // TODO: revert to main once PR is merged: https://github.com/open-feature/protocol/pull/56
-            spec: 'https://raw.githubusercontent.com/open-feature/protocol/aa6170c41418a034c8913677f23d69533e3804a8/service/openapi.yaml',
+            spec: 'https://raw.githubusercontent.com/open-feature/protocol/b549b94c91cf068d8068efaff7e74890e5238447/service/openapi.yaml',
           },
         ],
         theme: {
