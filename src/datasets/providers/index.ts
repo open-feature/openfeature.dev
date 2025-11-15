1068d110184e76207a875dc4b496cfd900f46667
Jared Wray <me@jaredwray.com>
2025-11-15 08:30:10 -0800

    feat: adding Hyphen AI to the provider ecosystem
    Signed-off-by: Jared Wray <me@jaredwray.com>

--- src/datasets/providers/index.ts
+++ src/datasets/providers/index.ts
@@ -14,6 +14,7 @@ import { Goff } from './goff';
 import { Harness } from './harness';
 import { Kameleoon } from './kameleoon';
 import { LaunchDarkly } from './launchdarkly';
+import { Hyphen } from './hyphen';
 import { MDBRules } from './mdb-rules';
 import { PostHog } from './posthog';
 import { Reflag } from './reflag';
@@ -66,6 +67,7 @@ export const PROVIDERS: Provider[] = [
   Hypertune,
   Kameleoon,
   LaunchDarkly,
+  Hyphen,
   MDBRules,
   PostHog,
   Reflag,
