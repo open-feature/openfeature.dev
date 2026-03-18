83e3776a2440191cf89457bd17692aada03e1908
Fredrik Lindskog <fredrik@lindskog.ch>
2026-03-18 18:26:04 +0100

    Refactor
    Signed-off-by: Fredrik Lindskog <fredrik@lindskog.ch>

--- src/datasets/providers/flipswitch.ts
+++ src/datasets/providers/flipswitch.ts
@@ -1,22 +1,18 @@
 import FlipswitchSvg from '@site/static/img/flipswitch-no-fill.svg';
 import type { Provider } from '.';
 
+const jsSdk = {
+  technology: 'JavaScript' as const,
+  vendorOfficial: true,
+  href: 'https://github.com/flipswitch-io/js-sdk',
+};
+
 export const Flipswitch: Provider = {
   name: 'Flipswitch',
   logo: FlipswitchSvg,
   technologies: [
-    {
-      technology: 'JavaScript',
-      vendorOfficial: true,
-      href: 'https://github.com/flipswitch-io/js-sdk',
-      category: ['Client'],
-    },
-    {
-      technology: 'JavaScript',
-      vendorOfficial: true,
-      href: 'https://github.com/flipswitch-io/js-sdk',
-      category: ['Server'],
-    },
+    { ...jsSdk, category: ['Client'] as const },
+    { ...jsSdk, category: ['Server'] as const },
     {
       technology: 'Java',
       vendorOfficial: true,
