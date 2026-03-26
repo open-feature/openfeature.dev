a43ed87a0b844adbd881c6ded9fa136618dc7489
Fredrik Lindskog <fredrik@lindskog.ch>
2026-03-26 21:34:26 +0100

    feat: extend Flipswitch SDK list and add OFREP API entry
    Signed-off-by: Fredrik Lindskog <fredrik@lindskog.ch>

---
+++ src/datasets/ofrep-api/flipswitch.ts
@@ -0,0 +1,9 @@
+import FlipswitchSvg from '@site/static/img/flipswitch-no-fill.svg';
+import { OFREP_API } from '.';
+
+export const Flipswitch: OFREP_API = {
+  name: 'Flipswitch',
+  logo: FlipswitchSvg,
+  href: 'https://docs.flipswitch.io/docs/sdks/ofrep',
+  vendorOfficial: true,
+};
