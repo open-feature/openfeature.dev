3676bfb705e57c414b64ec07e0aec271183ec4d0
Jonathan Norris <jonathan@taplytics.com>
2026-08-13 21:33:07 +0000

    feat: add Cloudflare Flagship providers (#1439)
    Adds Cloudflare Flagship to the OpenFeature provider ecosystem, covering
    official JavaScript client/server, Go, and Python providers. Includes a
    monochrome Cloudflare logo following the ecosystem SVG requirements.
    
    [Flagship announcement](https://blog.cloudflare.com/flagship/) ·
    [OpenFeature SDK docs](https://developers.cloudflare.com/flagship/sdk/)
    
    Signed-off-by: Jonathan Norris <jonathan.norris@dynatrace.com>

--- src/datasets/providers/index.ts
+++ src/datasets/providers/index.ts
@@ -4,6 +4,7 @@ import { ABTasty } from './abtasty';
 import { AwsSSM } from './awsssm';
 import { Bucketeer } from './bucketeer';
 import { CloudBees } from './cloudbees';
+import { Cloudflare } from './cloudflare';
 import { ConfigCat } from './configcat';
 import { ConfigDirector } from './configdirector';
 import { Datadog } from './datadog';
@@ -64,6 +65,7 @@ export const PROVIDERS: Provider[] = [
   AwsSSM,
   Bucketeer,
   CloudBees,
+  Cloudflare,
   Confidence,
   ConfigBee,
   ConfigCat,
